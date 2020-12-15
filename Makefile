668cfce40a9db3f9b13c0fd6839e4cc5201c4f0b
Einar Lönn <el@sunet.se>
2020-12-15 12:45:53 +0100

    Every good story begins with a Makefile, Dockerfile and a start script

---
+++ Makefile
@@ -0,0 +1,12 @@
+VERSION=7.3.5
+NAME=docker-grafana
+
+all: build push
+build:
+	docker build --no-cache=true -t $(NAME):$(VERSION) .
+	docker tag $(NAME):$(VERSION) docker.sunet.se/$(NAME):$(VERSION)
+update:
+	docker build -t $(NAME):$(VERSION) .
+	docker tag $(NAME):$(VERSION) docker.sunet.se/$(NAME):$(VERSION)
+push:
+	docker push docker.sunet.se/$(NAME):$(VERSION)
---
+++ start.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+PERMISSIONS_OK=0
+
+GF_PATHS_CONFIG="/etc/grafana/grafana.ini"
+GF_PATHS_DATA="/var/lib/grafana"
+GF_PATHS_HOME="/usr/share/grafana"
+GF_PATHS_PLUGINS="/var/lib/grafana/plugins"
+GF_PATHS_PROVISIONING="/etc/grafana/provisioning"
+
+
+if [ ! -r "$GF_PATHS_CONFIG" ]; then
+    echo "GF_PATHS_CONFIG='$GF_PATHS_CONFIG' is not readable."
+    PERMISSIONS_OK=1
+fi
+
+if [ ! -w "$GF_PATHS_DATA" ]; then
+    echo "GF_PATHS_DATA='$GF_PATHS_DATA' is not writable."
+    PERMISSIONS_OK=1
+fi
+
+if [ ! -r "$GF_PATHS_HOME" ]; then
+    echo "GF_PATHS_HOME='$GF_PATHS_HOME' is not readable."
+    PERMISSIONS_OK=1
+fi
+
+if [ $PERMISSIONS_OK -eq 1 ]; then
+    echo "You may have issues with file permissions, more information here: http://docs.grafana.org/installation/docker/#migration-from-a-previous-version-of-the-docker-container-to-5-1-or-later"
+fi
+
+if [ ! -d "$GF_PATHS_PLUGINS" ]; then
+    mkdir "$GF_PATHS_PLUGINS"
+fi
+
+ 
+exec grafana-server                                         \
+  --homepath="$GF_PATHS_HOME"                               \
+  --config="$GF_PATHS_CONFIG"                               \
+  --packaging=docker                                        \
+  "$@"                                                      \
+  cfg:default.log.mode="console"                            \
+  cfg:default.paths.data="$GF_PATHS_DATA"                   \
+  cfg:default.paths.logs="$GF_PATHS_LOGS"                   \
+  cfg:default.paths.plugins="$GF_PATHS_PLUGINS"             \
+  cfg:default.paths.provisioning="$GF_PATHS_PROVISIONING"
+
